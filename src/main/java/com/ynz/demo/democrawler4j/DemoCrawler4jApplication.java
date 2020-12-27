4be88df9a640034424af6dd2c0cd496f7601866d
yichunzhao <zhaoyichun@hotmail.com>
2020-12-27 16:54:14 +0100

    Initial commit

---
+++ src/main/java/com/ynz/demo/democrawler4j/DemoCrawler4jApplication.java
@@ -0,0 +1,13 @@
+package com.ynz.demo.democrawler4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCrawler4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCrawler4jApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ynz/demo/democrawler4j/DemoCrawler4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ynz.demo.democrawler4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCrawler4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
