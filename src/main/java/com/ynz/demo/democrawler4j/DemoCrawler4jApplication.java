1a40e6184c29d46de689facfc63fb9fadeaea30c
yichunzhao <zhaoyichun@hotmail.com>
2020-12-27 20:21:18 +0100

    test crawler for j

--- src/main/java/com/ynz/demo/democrawler4j/DemoCrawler4jApplication.java
+++ src/main/java/com/ynz/demo/democrawler4j/DemoCrawler4jApplication.java
@@ -1,13 +1,47 @@
 package com.ynz.demo.democrawler4j;
 
+import com.ynz.demo.democrawler4j.webcrawerler.HtmlCrawler;
+import edu.uci.ics.crawler4j.crawler.CrawlConfig;
+import edu.uci.ics.crawler4j.crawler.CrawlController;
+import edu.uci.ics.crawler4j.fetcher.PageFetcher;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+
 @SpringBootApplication
-public class DemoCrawler4jApplication {
+public class DemoCrawler4jApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoCrawler4jApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoCrawler4jApplication.class, args);
+    }
 
+    @Override
+    public void run(String... args) throws Exception {
+        File savedFiles = new File("src/test/resources/crawler4j");
+        CrawlConfig config = new CrawlConfig();
+        config.setCrawlStorageFolder(savedFiles.getAbsolutePath());
+        config.setMaxDepthOfCrawling(2);
+        config.setMaxPagesToFetch(500);
+        config.setPolitenessDelay(300);
+        config.setResumableCrawling(true);
+
+
+        int numCrawlers = Runtime.getRuntime().availableProcessors();
+
+        PageFetcher pageFetcher = new PageFetcher(config);
+        RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+        RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+        CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
+
+
+        controller.addSeed("https://finance.yahoo.com/quote/BFT?p=BFT&.tsrc=fin-srch");
+
+        CrawlController.WebCrawlerFactory<HtmlCrawler> factory = HtmlCrawler::new;
+        controller.start(factory, numCrawlers);
+
+    }
 }
---
+++ src/main/java/com/ynz/demo/democrawler4j/webcrawerler/HtmlCrawler.java
@@ -0,0 +1,46 @@
+package com.ynz.demo.democrawler4j.webcrawerler;
+
+import edu.uci.ics.crawler4j.crawler.Page;
+import edu.uci.ics.crawler4j.crawler.WebCrawler;
+import edu.uci.ics.crawler4j.parser.HtmlParseData;
+import edu.uci.ics.crawler4j.url.WebURL;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Set;
+import java.util.regex.Pattern;
+
+@Slf4j
+public class HtmlCrawler extends WebCrawler {
+
+    private final static Pattern EXCLUSIONS
+            = Pattern.compile(".*(\\.(css|js|xml|gif|jpg|png|mp3|mp4|zip|gz|pdf))$");
+
+    @Override
+    public boolean shouldVisit(Page referringPage, WebURL url) {
+        String urlString = url.getURL().toLowerCase();
+        return !EXCLUSIONS.matcher(urlString).matches()
+                && urlString.startsWith("https://finance.yahoo.com/");
+    }
+
+    @Override
+    public void visit(Page page) {
+        String url = page.getWebURL().getURL();
+        log.info("visited: ", url);
+
+        if (page.getParseData() instanceof HtmlParseData) {
+            HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
+            String title = htmlParseData.getTitle();
+            String text = htmlParseData.getText();
+            String html = htmlParseData.getHtml();
+            Set<WebURL> links = htmlParseData.getOutgoingUrls();
+
+            // do something with the collected data
+
+            log.info("title", title);
+            log.info("text: ", text);
+            log.info("html: ", html);
+
+        }
+    }
+
+}
